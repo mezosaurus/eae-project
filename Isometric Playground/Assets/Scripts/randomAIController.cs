084da0364defcf46a9bacb0424a39f312890e098
mezosaurus <ethanhayes72@gmail.com>
2014-09-15 15:29:46 -0600

    ai test

---
+++ Isometric Playground/Assets/Scripts/randomAIController.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+	
+}
+
+public class randomAIController : aiController 
+{
+	public int minMovementRepeat, maxMovementRepeat;
+	public Boundary boundary;
+
+	private float previousMovement;
+	private int moveCounter;
+
+	void FixedUpdate () 
+	{
+		// Five options for moving:
+		//    Up, Down, Left, Right, Still
+
+		float moveHorizontal = 0, moveVertical = 0, rand;
+
+		// Get random direction or continue on same path
+		rand = getRandomNumber ();
+
+		// Determine direction to move
+		if (rand < 1) 
+		{
+			if (transform.position.x + normalSpeed >= boundary.xMax)
+			{
+				previousMovement = 1.5f;
+				moveHorizontal = -1;
+			}
+			else
+			{
+				moveHorizontal = 1;
+			}
+		} 
+		else if (rand < 2) 
+		{
+			if (transform.position.x - normalSpeed <= boundary.xMin)
+			{
+				previousMovement = 0.5f;
+				moveHorizontal = 1;
+			}
+			else
+			{
+				moveHorizontal = -1;
+			}
+		}
+		else if (rand < 3) 
+		{
+			if (transform.position.z + normalSpeed >= boundary.zMax)
+			{
+				previousMovement = 3.5f;
+				moveVertical = -1;
+			}
+			else
+			{
+				moveVertical = 1;
+			}
+		}
+		else if (rand < 4) 
+		{
+			if (transform.position.z - normalSpeed <= boundary.zMin)
+			{
+				previousMovement = 2.5f;
+				moveVertical = 1;
+			}
+			else
+			{
+				moveVertical = -1;
+			}
+		}
+
+		// Apply random movement to AI
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.velocity = movement * normalSpeed;
+	}
+
+	float getRandomNumber()
+	{
+		float rand;
+		if (moveCounter == 0) 
+		{
+			moveCounter = Mathf.FloorToInt(Random.Range(minMovementRepeat,maxMovementRepeat));
+			rand = Random.Range (0, 5);
+			previousMovement = rand;
+		} 
+		else 
+		{
+			moveCounter--;
+			rand = previousMovement;
+		}
+
+		return rand;
+	}
+}
---
+++ Isometric Playground/Assets/Scripts/setAIController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setAIController : aiController
+{
+	//public float boundaryX, boundaryZ;
+	public Transform[] moveWayPoints;
+
+	private NavMeshAgent nav;
+	private int wayPointIndex;
+
+	void Start()
+	{
+		nav = GetComponent<NavMeshAgent> ();
+		wayPointIndex = moveWayPoints.Length - 1;
+	}
+
+	void Update () 
+	{
+		nav.speed = normalSpeed;
+
+		if(nav.remainingDistance == nav.stoppingDistance)
+		{
+			//Debug.Log("waypoint: " + wayPointIndex);
+			//Debug.Log ("last index: " + (moveWayPoints.Length - 1));
+			if (wayPointIndex == moveWayPoints.Length - 1)
+				wayPointIndex = 0;
+			else wayPointIndex++;
+		}
+
+		nav.destination = moveWayPoints [wayPointIndex].position;
+
+		// Move in clockwise fashion ( up Z, right X, down Z, left X)
+
+		//float moveHorizontal, moveVertical;
+
+		//if (rigidbody.position.z < boundaryZ)
+			//moveVertical = speed;
+		//else if (rigidbody.position
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		//rigidbody.velocity = movement * speed;
+	}
+}
