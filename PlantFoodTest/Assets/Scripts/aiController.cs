57f1fe26ccfff4337b03e5f0124b6ca287c8ae38
ckern <calvinmkern@gmail.com>
2014-09-17 09:58:33 -0600

    Updated background scene to allow for gravity, added the still ai that runs away (but not away from walls)

---
+++ PlantFoodTest/Assets/Scripts/aiController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aiController : MonoBehaviour 
+{
+	// Variables for alerting (alert radius, max alert level, ...)
+	public float nourishment;
+	public float normalSpeed;
+	public float runSpeed;
+	public GameObject player;
+
+	protected bool alerted;
+}
