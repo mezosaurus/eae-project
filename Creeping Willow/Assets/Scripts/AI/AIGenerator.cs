408084114f1fbe06f3db58254fad5788d5865f89
ckern <calvinmkern@gmail.com>
2014-10-27 09:45:02 -0600

    Added a simple spawn for NPC units.

---
+++ Creeping Willow/Assets/Scripts/AI/AIGenerator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIGenerator : MonoBehaviour 
+{
+	public GameObject npc;
+	public float spawnTime;
+
+	private float lastSpawnTime;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (lastSpawnTime <= Time.time - spawnTime)
+		{
+			lastSpawnTime = Time.time;
+
+			float xSpawn = getSpawnXPosition();
+			float ySpawn = getSpawnYPosition(xSpawn);
+
+			Instantiate (npc, new Vector3(xSpawn, ySpawn, 0), Quaternion.identity);
+		}
+	}
+
+	float getSpawnXPosition()
+	{
+		return -11;
+	}
+
+	float getSpawnYPosition(float xPosition)
+	{
+		Vector2 point1 = new Vector2 (-26.5f, 0.0f);
+		Vector2 point2 = new Vector2 (0.0f, 15.3f);
+
+		float slope = (point2.y - point1.y) / (point2.x - point1.x);
+		float intercept = point1.y - (slope * point1.x);
+
+		return (slope * xPosition) + intercept;
+	}
+}
