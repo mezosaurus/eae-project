607fbaefffbf305231973db34f1c7cd77acf801f
ColonelWittyHill <ctlillywhite@msn.com>
2014-10-26 19:20:24 -0600

    GUI - Added simple UI to my own test scene.

---
+++ Creeping Willow/Assets/Scripts/GUI/CycleTimer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CycleTimer : MonoBehaviour
+{
+	public Texture2D back;
+	public Texture2D cover;
+
+	public int left = 10;
+	public int top = 10;
+	public int width = 50;
+	public int height = 50;
+
+	private int angle = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		angle++;
+	}
+
+	void OnGUI ()
+	{
+		// draw the day/night cycle
+		Matrix4x4 oldMatrix = GUI.matrix;
+		GUIUtility.RotateAroundPivot (angle, new Vector2 (left + width / 2, top + height / 2));
+		GUI.DrawTexture (new Rect (left, top, width, height), back);
+		GUI.matrix = oldMatrix;
+
+		// draw the cover
+		GUI.DrawTexture (new Rect (left, top, width, height), cover);
+	}
+}
