630e7a0127b439e9cb625128ed76eb4a45d41b24
global winksGeek <global dwinkler426@gmail.com>
2014-10-28 15:33:14 -0600

    Added lure and trap

---
+++ Creeping Willow/Assets/Scripts/Abilities/Lure/Lure.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lure : AbilityClass {
+
+	public int lurePower;
+
+	// Use this for initialization
+	void Start () {
+		//LureSpawned
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check for npc in radius and broadcast entered lure
+	}
+
+	void onDestroy(){
+
+	}
+}
