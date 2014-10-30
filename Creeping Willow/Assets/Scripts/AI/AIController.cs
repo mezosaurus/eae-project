4fe980d02484eed1b708e49ccce9e0d4dafdd57d
ckern <calvinmkern@gmail.com>
2014-10-30 12:20:54 -0600

    Added test scene for ai testing.

--- Creeping Willow/Assets/Scripts/AI/AIController.cs
+++ Creeping Willow/Assets/Scripts/AI/AIController.cs
@@ -3,23 +3,44 @@ using System.Collections;
 
 public class AIController : MonoBehaviour {
 
-	private Vector3 moveDir;
 	public float speed;
-	private Vector3 spawnMove;
+	private GameObject nextPath;
+	private Vector3 moveDir;
+	//private Vector3 spawnMove;
+	private SubpathScript movePath;
+	private bool killSelf = false;
 
 	// Use this for initialization
 	void Start () {
-		spawnMove = GameObject.Find ("SpawnMove1").transform.position;
+		movePath = GameObject.Find ("Paths").GetComponent<PathingScript> ().getRandomPath().GetComponent<SubpathScript>();
+		nextPath = movePath.getNextPath (null);
+		//spawnMove = GameObject.Find ("Path1Point1").transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Vector3 pathPosition = nextPath.transform.position;
+		if (killSelf)
+		{
+			pathPosition = GameObject.FindGameObjectWithTag("Respawn").transform.position;
+		}
 		Vector3 position = transform.position;
 		//Vector3 goal = GameObject.Find ("SpawnMoves/SpawnMove1").transform.position;
 		float step = speed * Time.deltaTime;
-		Vector3 movement = Vector3.MoveTowards (position, spawnMove, step);
+		Vector3 movement = Vector3.MoveTowards (position, pathPosition, step);
+		//Vector3 movement = Vector3.MoveTowards (position, spawnMove, step);
 		transform.position = movement;
-		if (movement == spawnMove)
-			Destroy (gameObject);
+		if (movement == pathPosition)
+		{
+			if (killSelf)
+				Destroy(gameObject);
+
+			int max = 10;
+			int rand = Random.Range (0, max);
+			if (rand < max - 1)
+				nextPath = movePath.getNextPath(nextPath);
+			else
+				killSelf = true;
+		}
 	}
 }
---
+++ Creeping Willow/Assets/Scripts/AI/Pathing/SubpathScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubpathScript : MonoBehaviour 
+{
+	public GameObject[] paths;
+
+	public GameObject getNextPath(GameObject currentPath)
+	{
+		if (currentPath == null) 
+		{
+			return paths[0];
+		}
+		int index = 0;
+		for (int i = 0; i < paths.Length ; i++)
+		{
+			if (paths[i] == currentPath)
+			{
+				index = i;
+				break;
+			}
+		}
+		index = (index + 1) % paths.Length;
+		return paths[index];
+	}
+}
