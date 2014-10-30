7ec2d8c5288831187d09d072cc2b451d9c504d8f
ckern <calvinmkern@gmail.com>
2014-10-30 14:38:25 -0600

    Fixed errors in AI scripts.

--- Creeping Willow/Assets/Scripts/AI/AIController.cs
+++ Creeping Willow/Assets/Scripts/AI/AIController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class AIController : MonoBehaviour {
 
+	/*
 <<<<<<< HEAD
 	//private Vector3 moveDir;
 	public float speed;
@@ -144,7 +145,7 @@ public class AIController : MonoBehaviour {
 <<<<<<< HEAD
 		if (movement == spawnMove)
 			Destroy (gameObject);
-	}*/
+	}/
 =======
 		if (movement == pathPosition)
 		{
@@ -160,4 +161,74 @@ public class AIController : MonoBehaviour {
 		}
 	}
 >>>>>>> 358724c430cf43ab3cd43c416f20474ae2df15bb
+	*/
+
+	// Copied variables
+	public float speed;
+	private Vector3 spawnMove;
+	public float nourishment;
+	public float normalSpeed;
+	public float runSpeed;
+	public float alertTimer;
+	public GameObject player;
+	
+	public AudioClip chaseMusic;
+	
+	public Sprite normalTexture;
+	public Sprite alertTexture;
+	public Sprite panicTexture;
+	
+	public bool alerted;
+	public bool panicked;
+	public bool grabbed;
+	
+	protected bool nearWall;
+	protected Vector2 moveDir; 
+	protected float alertedTime;
+	protected float panicTime;
+	public float panicCooldown;
+	protected float timePanicked;
+
+	// New Variables
+	//public float speed;
+	private GameObject nextPath;
+	//private Vector3 moveDir;
+	//private Vector3 spawnMove;
+	private SubpathScript movePath;
+	private bool killSelf = false;
+	
+	// Use this for initialization
+	public void Start () {
+		movePath = GameObject.Find ("Paths").GetComponent<PathingScript> ().getRandomPath().GetComponent<SubpathScript>();
+		nextPath = movePath.getNextPath (null);
+		//spawnMove = GameObject.Find ("Path1Point1").transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pathPosition = nextPath.transform.position;
+		if (killSelf)
+		{
+			pathPosition = GameObject.FindGameObjectWithTag("Respawn").transform.position;
+		}
+		Vector3 position = transform.position;
+		//Vector3 goal = GameObject.Find ("SpawnMoves/SpawnMove1").transform.position;
+		float step = speed * Time.deltaTime;
+		Vector3 movement = Vector3.MoveTowards (position, pathPosition, step);
+		//Vector3 movement = Vector3.MoveTowards (position, spawnMove, step);
+		transform.position = movement;
+		if (movement == pathPosition)
+		{
+			if (killSelf)
+				Destroy(gameObject);
+			
+			int max = 10;
+			int rand = Random.Range (0, max);
+			if (rand < max - 1)
+				nextPath = movePath.getNextPath(nextPath);
+			else
+				killSelf = true;
+		}
+	}
+
 }
