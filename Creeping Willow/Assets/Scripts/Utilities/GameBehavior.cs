c6d3ff8f1c130a08efbe9808c1ff04cf026d5c88
ColonelWittyHill <ctlillywhite@msn.com>
2014-11-13 07:46:32 -0700

    Timer - Updated the day/night cycle and timer GUI.

--- Creeping Willow/Assets/Scripts/Utilities/GameBehavior.cs
+++ Creeping Willow/Assets/Scripts/Utilities/GameBehavior.cs
@@ -6,25 +6,40 @@ public class GameBehavior : MonoBehaviour
 	protected bool paused = false;
 	bool skipFrame = false;
 
+	/// <summary>
+	/// The system time at the beginning of the update call.  Only use in Gameupdate!  Do not modify!
+	/// </summary>
+	protected float g_currentTime = 0.0f;
+
+	/// <summary>
+	/// The system time at the end of the previous update call.  Only use in Gameupdate!  Do not modify!
+	/// </summary>
+	protected float g_previousTime = 0.0f;
+
 	void Awake()
 	{
 		MessageCenter.Instance.RegisterListener (MessageType.PauseChanged, HandlePauseChanged);
 	}
 
-    private void Update()
-    {
-        // Check to see if paused
+	private void Update()
+	{
+		g_currentTime = Time.time;
+
+		// Check to see if paused
 		if( !paused )
 		{
 			// This is used to invalidate any getButtonDown inputs from exiting the pause menu
 			if( skipFrame ) 
 				skipFrame = false;
 			else
-        		GameUpdate();
+				GameUpdate();
 		}
-    }
 
-    protected virtual void GameUpdate() { }
+		g_previousTime = g_currentTime;
+		}
+	}
+
+	protected virtual void GameUpdate() { }
 
 	private void OnDestroy()
 	{
