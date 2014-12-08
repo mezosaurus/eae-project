352ad7adf9a1cf7a0be9ab357a3bcb26c8d1a8f5
mezosaurus <ethanhayes72@gmail.com>
2014-12-08 15:20:22 -0700

    fixed alert level decrement so they remain alerted even if player is standing still, or if they move away from player

---
+++ Creeping Willow/Assets/Scripts/GUI/ControlsScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlsScript : MonoBehaviour {
+
+	public GUITexture controlsMap;
+	public bool nextBtnTapped;
+
+	// Use this for initialization
+	void Start () {
+		nextBtnTapped = false;
+		controlsMap.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("A"))
+		{
+			controlsMap.enabled = false;
+			nextBtnTapped = true;
+		}
+	}
+}
