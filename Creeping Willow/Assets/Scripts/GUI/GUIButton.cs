bd60f9d589cfa8cfea107b717d205f55433c201d
ColonelWittyHill <ctlillywhite@msn.com>
2014-12-11 13:39:31 -0700

    Menu Buttons now scale properly.

--- Creeping Willow/Assets/Scripts/GUI/GUIButton.cs
+++ Creeping Willow/Assets/Scripts/GUI/GUIButton.cs
@@ -26,6 +26,8 @@ public class GUIButton : MonoBehaviour
 
 	void OnGUI()
 	{
+		GUI.matrix = GlobalGameStateManager.PrepareMatrix();
+
 		// set the GUI images and font
 		GUI.skin.button.normal.background = (Texture2D)defaultImage;
 		GUI.skin.button.hover.background = (Texture2D)hoverImage;
@@ -38,6 +40,8 @@ public class GUIButton : MonoBehaviour
 		// draw the button
 		if( GUI.Button( new Rect( x - width / 2, y - height / 2, width, height ), text ) )
 				changeScenes();
+
+		GUI.matrix = Matrix4x4.identity;
 	}
 
 	public void changeScenes()
