3df59d37394eb1d03b9639f21231bb4ec6b56013
ColonelWittyHill <ctlillywhite@msn.com>
2015-01-14 12:34:40 -0700

    InteractiveMenu started.

--- Creeping Willow/Assets/Scripts/GUI/GUIButton.cs
+++ Creeping Willow/Assets/Scripts/GUI/GUIButton.cs
@@ -9,6 +9,9 @@ using System.Collections;
 /// </summary>
 public class GUIButton : MonoBehaviour
 {
+	public delegate void ButtonClickFunction();
+
+	private ButtonClickFunction clickFunction;
 	public float x, y, width, height;
 	public Texture2D defaultImage, hoverImage, downClickImage;
 	public string text;
@@ -16,6 +19,8 @@ public class GUIButton : MonoBehaviour
 	public SceneEnum scene;
 	public AudioClip sound;
 	public bool changeScene = true;
+	public bool selected = false;
+	public float fadeTime;
 
 	private AudioSource audio;
 
@@ -23,26 +28,41 @@ public class GUIButton : MonoBehaviour
 	{
 		audio = gameObject.AddComponent<AudioSource> ();
 		audio.clip = sound;
+
+		//clickFunction = changeScenes;
 	}
 
 	void OnGUI()
 	{
-		GUI.matrix = GlobalGameStateManager.PrepareMatrix();
+		if( enabled )
+		{
+			//GUI.matrix = GlobalGameStateManager.PrepareMatrix();
 
-		// set the GUI images and font
-		GUI.skin.button.normal.background = (Texture2D)defaultImage;
-		GUI.skin.button.hover.background = (Texture2D)hoverImage;
-		GUI.skin.button.active.background = (Texture2D)defaultImage;
-		GUI.skin.font = font;
-		GUI.skin.GetStyle( "Button" ).fontSize = Mathf.FloorToInt( 0.6f * height );
-		GUI.skin.GetStyle( "Button" ).normal.textColor = Color.black;
-		GUI.depth = 0;
+			// set the GUI images and font
+			GUI.skin.button.normal.background = ( Texture2D )defaultImage;
+			GUI.skin.button.hover.background = ( Texture2D )hoverImage;
+			GUI.skin.button.active.background = ( Texture2D )defaultImage;
+			GUI.skin.font = font;
+			GUI.skin.GetStyle( "Button" ).fontSize = Mathf.FloorToInt( 0.6f * height );
+			GUI.skin.GetStyle( "Button" ).normal.textColor = Color.black;
+			GUI.depth = 0;
 
-		// draw the button
-		if( GUI.Button( new Rect( x - width / 2, y - height / 2, width, height ), text ) )
-				changeScenes();
+			// draw the button
+			if( GUI.Button( new Rect( x - width / 2, y - height / 2, width, height ), text ) )
+					ClickButton();
 
-		GUI.matrix = Matrix4x4.identity;
+			//GUI.matrix = Matrix4x4.identity;
+		}
+	}
+
+	public void SetClickAction( ButtonClickFunction i_function )
+	{
+		this.clickFunction = i_function;
+	}
+
+	public void ClickButton()
+	{
+		clickFunction();
 	}
 
 	public void changeScenes()
@@ -52,6 +72,12 @@ public class GUIButton : MonoBehaviour
 			audio.Play();
 			Invoke( "switchScenes", 0.5f );
 		}
+	}
+
+	public void Fade()
+	{
+		//TODO play some animation
+		enabled = false;
 	}
 
 	void switchScenes ()
---
+++ Creeping Willow/Assets/Scripts/GUI/InteractiveMenuController.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractiveMenuController : MonoBehaviour
+{
+	public GameObject[] buttonObjects;
+	public Vector3[] buttonPositions;
+	public AudioClip clickSound;
+	public Texture2D cursorImage;
+	public GameObject camera;
+
+	private bool axisBusy;
+	private int selected;
+	private bool usesSound;
+	private AudioSource clickAudio;
+	private Vector3 cameraPosition;
+	private float totalZoomTime;
+	private float elapsedZoomTime;
+
+	void Awake()
+	{
+		selected = 0;
+		axisBusy = false;
+
+		if( clickSound )
+		{
+			usesSound = true;
+			clickAudio = gameObject.AddComponent<AudioSource>();
+			clickAudio.clip = clickSound;
+		}
+
+		selected = -1;
+
+		if( buttonObjects.Length <= 0 )
+			throw new UnassignedReferenceException( "No button was given to the menu" );
+
+		buttonObjects[ 0 ].GetComponent<GUIButton>().SetClickAction( ZoomCameraToLevelSelect );
+
+		if( cursorImage )
+			Cursor.SetCursor( cursorImage, new Vector2( cursorImage.width / 2, cursorImage.height / 2 ), CursorMode.ForceSoftware );
+
+		totalZoomTime = 3.0f;
+		cameraPosition = new Vector3( 0, 0, -10 );
+	}
+
+	void OnDestroy() {}
+
+	public void ZoomCameraToLevelSelect()
+	{
+		// Get rid of all the front buttons
+		buttonObjects[ 0 ].GetComponent<GUIButton>().Fade();
+		buttonObjects[ 1 ].GetComponent<GUIButton>().Fade();
+		buttonObjects[ 2 ].GetComponent<GUIButton>().Fade();
+
+		// Set the next camera position
+		cameraPosition = new Vector3( 0, 0, 0 );
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// Check for mouse
+		if( Input.GetAxis( "Mouse X" ) != 0 || Input.GetAxis( "Mouse Y" ) != 0 )
+		{
+			Screen.showCursor = true;
+			Screen.lockCursor = false;
+			axisBusy = true;
+			selected = -1;
+			UnselectAll();
+		}
+
+		// Check for controller movement
+		else if( Input.GetAxis( "LSX" ) != 0 || Input.GetAxis( "DX" ) != 0 )
+		{
+			if( !axisBusy )
+			{
+				if( Input.GetAxisRaw( "LSX" ) < 0 || Input.GetAxisRaw( "DX" ) < 0 )
+					selected--;
+				else if( Input.GetAxisRaw( "LSX" ) > 0 || Input.GetAxisRaw( "DX" ) > 0 )
+					selected++;
+
+				if( selected < 0 )
+					selected = buttonObjects.Length - 1;
+				else if( selected > buttonObjects.Length - 1 )
+					selected = 0;
+
+				Screen.showCursor = false;
+				Screen.lockCursor = true;
+				axisBusy = true;
+				Select( buttonObjects[ selected ] );
+			}
+		}
+		else
+			axisBusy = false;
+
+		// The user wants to go forward
+		if( Input.GetAxisRaw( "Start" ) != 0 || Input.GetAxisRaw( "A" ) != 0 )
+		{
+			if( selected >= 0 )
+				buttonObjects[ selected ].GetComponent<GUIButton>().ClickButton();
+		}
+		// The user wants to go back
+		else if( Input.GetAxisRaw( "Back" ) != 0 || Input.GetAxisRaw( "B" ) != 0 )
+		{
+			//TODO exit the program or ask to quit or something
+		}
+
+		// Move the camera to the correct position
+		if( camera.transform.position != cameraPosition )
+		{
+			elapsedZoomTime += Time.deltaTime;
+			camera.transform.position = Vector3.Lerp( camera.transform.position, cameraPosition, elapsedZoomTime / totalZoomTime );
+		}
+		else
+			elapsedZoomTime = 0.0f;
+	}
+
+	private void Select( GameObject button )
+	{
+		UnselectAll ();
+
+		button.GetComponent<GUIButton>().defaultImage = button.GetComponent<GUIButton>().hoverImage;
+		if( usesSound )
+			clickAudio.PlayOneShot( clickSound );
+	}
+
+	private void UnselectAll()
+	{
+		foreach( GameObject button in buttonObjects )
+			button.GetComponent<GUIButton>().defaultImage = button.GetComponent<GUIButton>().downClickImage;
+	}
+}
