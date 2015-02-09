fe9fa82166e69324242eeb2dc51f4e7b8562fb84
ColonelWittyHill <ctlillywhite@msn.com>
2015-02-08 17:45:02 -0700

    Added a blank LoadingScreen.  This can also be used to pass information in from the menu to the level.

---
+++ Creeping Willow/Assets/Scripts/GUI/LevelLoader.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+	public string levelName;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
