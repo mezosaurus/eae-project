83a321c9aae3713e7c1e27dfa8695d36131b22a5
azettler <azlotr16@gmail.com>
2015-02-24 02:32:20 -0700

    added in notoriety bar, some failed font testing

---
+++ Creeping Willow/Assets/Scripts/CustomFontScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomFontScript : MonoBehaviour {
+
+	public Font myFont;
+	GUIStyle myStyle = new GUIStyle ();
+
+	// Use this for initialization
+	void Start () {
+		myStyle.font = myFont;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (300, 200, 400, 50), "asdfghjklqwertyuiopzxcvbnm", myStyle);
+	}
+}
--- Creeping Willow/Assets/Scripts/GUI/NotorietyMeter.cs
+++ Creeping Willow/Assets/Scripts/GUI/NotorietyMeter.cs
@@ -14,11 +14,35 @@ public class NotorietyMeter : MonoBehaviour
 	private float top;
 	private float left;
 
+	public Texture2D bar1;
+	public Texture2D bar2;
+	public Texture2D bar3;
+	public Texture2D bar4;
+	public Texture2D bar5;
+	public Texture2D bar6;
+	public Texture2D bar7;
+	public Texture2D bar8;
+	public Texture2D bar9;
+	public Texture2D bar10;
+	public Texture2D bar11;
+	public Texture2D bar12;
+	public Texture2D bar13;
+	public Texture2D bar14;
+	public Texture2D bar15;
+	public Texture2D bar16;
+	public Texture2D bar17;
+	public Texture2D bar18;
+	public Texture2D bar19;
+	public Texture2D bar20;
+	public Texture2D bar21;
+	public Texture2D bar22;
+
+
 	void Start()
 	{
 		RegisterListeners();
 
-		width = 80;
+		width = 160;
 		height = 800;
 		
 		top = GlobalGameStateManager.originalHeight / 2 - height / 2;
@@ -56,6 +80,62 @@ public class NotorietyMeter : MonoBehaviour
 		}
 	}
 
+	Texture2D getMultiplierBarImage(float num, float total)
+	{
+		//Debug.Log ("num image: " + num);
+		
+		switch((int)Mathf.Round((num/total)*21))
+		{
+		case 0:
+			return bar22;
+		case 1:
+			return bar21;
+		case 2:
+			return bar20;
+		case 3:
+			return bar19;
+		case 4:
+			return bar18;
+		case 5:
+			return bar17;
+		case 6:
+			return bar16;
+		case 7:
+			return bar15;
+		case 8:
+			return bar14;
+		case 9:
+			return bar13;
+		case 10:
+			return bar12;
+		case 11:
+			return bar11;
+		case 12:
+			return bar10;
+		case 13:
+			return bar9;
+		case 14:
+			return bar8;
+		case 15:
+			return bar7;
+		case 16:
+			return bar6;
+		case 17:
+			return bar5;
+		case 18:
+			return bar4;
+		case 19:
+			return bar3;
+		case 20:
+			return bar2;
+		case 21:
+			return bar1;
+		default:
+			return bar22;
+		}
+		
+	}
+
 	/*
 	protected void HandleNPCAlertMessage( Message message )
 	{
@@ -90,8 +170,9 @@ public class NotorietyMeter : MonoBehaviour
 
 		float percentFull = notoriety / notorietyMax;
 
-		GUI.Box( new Rect( left, top, width, height ), GUIContent.none );
-		GUI.Box( new Rect( left, top + height * ( 1 - percentFull ), width, height * percentFull ), GUIContent.none );
+		//GUI.Box( new Rect( left, top, width, height ), GUIContent.none );
+		//GUI.Box( new Rect( left, top + height * ( 1 - percentFull ), width, height * percentFull ), GUIContent.none );
+		GUI.DrawTexture (new Rect (left, top, width, height), getMultiplierBarImage(notoriety,notorietyMax));
 
         // Matt changed this, feel free to change back
         float w = 80f;
