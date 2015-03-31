af5e4d04fac07e1a767ee9a4742ed3085abf70fe
winksGeek <dwinkler426@gmail.com>
2015-03-30 19:56:22 -0600

    bush animation tests

---
+++ Creeping Willow/Assets/Scripts/Abilities/Possession/RoundBushPossessable.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoundBushPossessable : PossessableItem
+{
+
+    protected override void Start()
+    {
+        base.Start();
+        hintOffset = new Vector2(.25f, .85f);
+    }
+
+
+    // Update is called once per frame
+    protected override void GameUpdate()
+    {
+        base.GameUpdate();
+    }
+
+    protected override void lure()
+    {
+        base.lure();
+        blinking = true;
+        AbilityPlacedMessage message = new AbilityPlacedMessage(transform.position.x, transform.position.y, AbilityType.PossessionLure);
+        MessageCenter.Instance.Broadcast(message);
+    }
+
+    protected override void scare()
+    {
+        base.scare();
+        shaking = true;
+        AbilityPlacedMessage message = new AbilityPlacedMessage(transform.position.x, transform.position.y, AbilityType.PossessionScare);
+        MessageCenter.Instance.Broadcast(message);
+        //GamePad.SetVibration(PlayerIndex.One, 1f, 1f);
+    }
+
+    public override void possess()
+    {
+        base.possess();
+        animation.Play("possessbush3_3");
+    }
+}
