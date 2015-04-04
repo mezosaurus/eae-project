ddc912d1cf2940b25c716cbeb42836a4b80b9f9f
global winksGeek <global dwinkler426@gmail.com>
2015-04-04 13:39:24 -0600

    Animated bushes

--- Creeping Willow/Assets/Scripts/Abilities/Possession/BushPossessable.cs
+++ Creeping Willow/Assets/Scripts/Abilities/Possession/BushPossessable.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RoundBushPossessable : PossessableItem
+public class BushPossessable : PossessableItem
 {
 
     protected override void Start()
