fa46a7605ad31b77d57074dcd51c4638bdac1911
azettler <azlotr16@gmail.com>
2015-04-05 15:29:52 -0600

    added new messages and changed some scoring functionality

---
+++ Creeping Willow/Assets/Scripts/Utilities/Messaging/Message Subclasses/MarkedBountyDestroyedMessage.cs
@@ -0,0 +1,11 @@
+﻿/**
+ * This message can be used to tell when a marked bounty npc is destroyed by being eaten or escaping
+ **/
+using UnityEngine;
+public class MarkedBountyDestroyedMessage : Message
+{
+	public MarkedBountyDestroyedMessage ():base(MessageType.MarkedBountyDestroyed)
+	{
+
+	}
+}
