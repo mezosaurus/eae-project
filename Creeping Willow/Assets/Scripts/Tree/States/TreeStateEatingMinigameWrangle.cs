8fba500b56ebe655a95a3d998b0df5836e0c681a
lawtonmatthewe <lawtonmatthewe@gmail.com>
2015-04-25 19:30:25 -0600

    Dynamic zoom while eating NPC's

--- Creeping Willow/Assets/Scripts/Tree/States/TreeStateEatingMinigameWrangle.cs
+++ Creeping Willow/Assets/Scripts/Tree/States/TreeStateEatingMinigameWrangle.cs
@@ -173,6 +173,10 @@ public class TreeStateEatingMinigameWrangle : TreeState
 
         Tree.BodyParts.MinigameCircle.GetComponent<SpriteRenderer>().sprite = Tree.Sprites.EatingMinigame.Circle[percentage];
 
+        float cameraSize = Mathf.Lerp(1.8f, 1.2f, progress);
+        Camera.main.orthographicSize = cameraSize;
+        Camera.main.GetComponent<CameraScript>().TargetSize = cameraSize;
+
         // To-do
         if (timePercentage >= 1f)
         {
