3c16dbdc2474b9a5f54975f93d37997d357ef0ba
calvinkern <calvinkern@gmail.com>
2015-04-26 13:12:12 -0600

    Stupid Unity Script.

---
+++ stupidUnity.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git add -A
+git reset Creeping\ Willow/Assets/UnityVS/Editor/UnityVS.VersionSpecific.dll
+git reset Creeping\ Willow/Assets/Scene/Sprint3_Levels/Evan_Level1.unity
+git checkout -- .
+git status
