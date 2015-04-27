587bb18752472018d55001183ba2bdda905eb398
azettler <azlotr16@gmail.com>
2015-04-27 00:13:58 -0600

    testing something

---
+++ Creeping Willow/Assets/Scripts/GetSavedHighScoresScript.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GetSavedHighScoresScript {
+
+	public static string[] LoadStalkSurvivorNames()
+	{
+		string[] names = new string[10];
+		
+		for( int i = 0; i < 10; i++ )
+		{
+			names[i] = PlayerPrefs.GetString(Application.loadedLevelName + "_" + LevelLoader.instance.modeName + "_name_" + i, "");
+		}
+
+		return names;
+	}
+
+	public static int[] LoadStalkSurvivorScores()
+	{
+		int[] scores = new int[10];
+		
+		for( int i = 0; i < 10; i++ )
+		{
+			scores[i] = PlayerPrefs.GetInt(Application.loadedLevelName + "_" + LevelLoader.instance.modeName + "_score_" + i, 0);
+		}
+		
+		return scores;
+	}
+
+	public static string[] LoadStalkFeastNames()
+	{
+		string[] names = new string[10];
+		
+		for( int i = 0; i < 10; i++ )
+		{
+			names[i] = PlayerPrefs.GetString(Application.loadedLevelName + "_" + LevelLoader.instance.modeName + "_name_" + i, "");
+		}
+		
+		return names;
+	}
+
+	public static int[] LoadStalkFeastScores()
+	{
+		int[] scores = new int[10];
+		
+		for( int i = 0; i < 10; i++ )
+		{
+			scores[i] = PlayerPrefs.GetInt(Application.loadedLevelName + "_" + LevelLoader.instance.modeName + "_score_" + i, 0);
+		}
+		
+		return scores;
+	}
+
+	public static string[] LoadStalkMarkedNames()
+	{
+		string[] names = new string[10];
+		
+		for( int i = 0; i < 10; i++ )
+		{
+			names[i] = PlayerPrefs.GetString(Application.loadedLevelName + "_" + LevelLoader.instance.modeName + "_name_" + i, "");
+		}
+		
+		return names;
+	}
+
+	public static int[] LoadStalkMarkedScores()
+	{
+		int[] scores = new int[10];
+		
+		for( int i = 0; i < 10; i++ )
+		{
+			scores[i] = PlayerPrefs.GetInt(Application.loadedLevelName + "_" + LevelLoader.instance.modeName + "_score_" + i, 0);
+		}
+		
+		return scores;
+	}
+}
